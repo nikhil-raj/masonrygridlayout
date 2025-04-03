936300c2e7cc9a97f12d7fda8d06f11c710b182a
Nikhil Raj <meetnikhilraj@gmail.com>
2025-04-03 23:24:46 +0200

    Set up React and React Router

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Photo Grid</header>
+    </div>
+  );
+}
+
+export default App;
